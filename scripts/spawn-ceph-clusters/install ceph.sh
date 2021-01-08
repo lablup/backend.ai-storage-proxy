fd45831e1d2f212db162e51e7f1e12d883cb8503
Sergey Leksikov <lexikovs@gmail.com>
2021-01-08 15:58:51 +0900

    feat: install script for ceph and storage proxy

---
+++ scripts/spawn-ceph-clusters/install ceph.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+sudo chmod 755 /
+mkdir test-cluster && cd test-cluster
+ssh-keyscan 172.21.12.10 172.21.12.11 172.21.12.12 172.21.12.13
+ssh-keyscan ceph-server-1 ceph-server-2 ceph-server-3 ceph-server-client
+yes | ceph-deploy new ceph-server-1 ceph-server-2 ceph-server-3
+yes | ceph-deploy install --release=octopus ceph-admin ceph-server-1 ceph-server-2 ceph-server-3 ceph-client
+echo "1.===== deployed"
+ceph-deploy mon create-initial
+ssh ceph-server-2 "sudo mkdir /var/local/osd0 && sudo chown ceph:ceph /var/local/osd0"
+ssh ceph-server-3 "sudo mkdir /var/local/osd1 && sudo chown ceph:ceph /var/local/osd1"
+
+ceph-deploy osd prepare ceph-server-2:/var/local/osd0 ceph-server-3:/var/local/osd1
+ceph-deploy osd activate ceph-server-2:/var/local/osd0 ceph-server-3:/var/local/osd1
+
+yes | ceph-deploy admin ceph-admin ceph-server-1 ceph-server-2 ceph-server-3 ceph-client
+echo "2.===== deployed"
+sudo chmod +r /etc/ceph/ceph.client.admin.keyring
+ssh ceph-server-1 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
+ssh ceph-server-2 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
+ssh ceph-server-3 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
+ssh ceph-client   sudo chmod +r /etc/ceph/ceph.client.admin.keyring
+echo "3.===== deployed"
+ceph-deploy mgr create ceph-admin:mon_mgr
+
+ceph-deploy mds create ceph-server-1
+
+ceph osd pool create rbd 150 150
+
+sudo apt-get -y install ceph-fuse
+echo "Done"
+exit
---
+++ scripts/spawn-ceph-clusters/install_storage_proxy.sh
@@ -0,0 +1,44 @@
+
+sudo chmod 755 /
+cd $HOME/
+
+sudo apt install -y python3.8
+
+sudo apt-get install -y build-essential git-core libreadline-dev libsqlite3-dev libssl-dev libbz2-dev tk-dev libzmq3-dev libsnappy-dev libffi-dev
+
+sudo git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+sudo git clone https://github.com/pyenv/pyenv-virtualenv.git "$HOME/.pyenv/plugins/pyenv-virtualenv"
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashers
+sudo chmod 777 -R .pyenv/
+exec $SHELL -l
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv install 3.8.6
+pyenv virtualenv 3.8.6 storage-proxy
+pyenv global storage-proxy
+sudo git clone https://github.com/lablup/backend.ai-storage-agent /vagrant/storage-proxy/
+cd /vagrant/storage-proxy/
+pyenv local storage-proxy
+
+pip install backend.ai-common
+# python3.8 -m pip install --upgrade pip
+pip install -U -r requirements/dev.txt
+cp /vagrant/storage-proxy.toml storage-proxy.toml
+sudo apt-get -y install ceph-fuse
+sudo apt-get -y install attr
+
+sudo mkdir /mnt/vfroot/
+sudo mkdir /mnt/vfroot/ceph-fuse/
+sudo ceph-fuse -n client.admin --keyring=/etc/ceph/ceph.client.admin.keyring  -m ceph-server-1 /mnt/vfroot/ceph-fuse/ &
+python -m ai.backend.storage.server &
+exit
