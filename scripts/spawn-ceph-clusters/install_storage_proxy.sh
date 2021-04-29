f69ea5efcec865b7af7daa0273d82b85ffdd5236
Sergey Leksikov <lexikovs@gmail.com>
2021-04-29 16:59:38 +0900

    Update: install storage
    Update: install storage

--- scripts/spawn-ceph-clusters/install_storage_proxy.sh
+++ scripts/spawn-ceph-clusters/install_storage_proxy.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 
 sudo chmod 755 /
 cd $HOME/
@@ -6,11 +8,7 @@ sudo apt install -y python3.8
 
 sudo apt-get install -y build-essential git-core libreadline-dev libsqlite3-dev libssl-dev libbz2-dev tk-dev libzmq3-dev libsnappy-dev libffi-dev
 
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-exec $SHELL
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
+sudo git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init -)"' >> ~/.profile
@@ -23,18 +21,20 @@ sudo git clone https://github.com/pyenv/pyenv-virtualenv.git "$HOME/.pyenv/plugi
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.profile
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrs
 sudo chmod 777 -R .pyenv/
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
-pyenv install 3.8.6
-pyenv virtualenv 3.8.6 storage-proxy
+pyenv install 3.9.4
+pyenv virtualenv 3.9.4 storage-proxy
 pyenv global storage-proxy
 sudo git clone https://github.com/lablup/backend.ai-storage-agent /vagrant/storage-proxy/
 cd /vagrant/storage-proxy/
 pyenv local storage-proxy
 
+python3.9 -m pip install --upgrade pip
 pip install backend.ai-common
-# python3.8 -m pip install --upgrade pip
 pip install -U -r requirements/dev.txt
-cp /vagrant/storage-proxy.toml storage-proxy.toml
+
 sudo apt-get -y install ceph-fuse
 sudo apt-get -y install attr
 
