a1ea12cb7320823bef880a4c3e3b0a0481629a98
Jonghyun Park <adrysn@gmail.com>
2021-10-18 17:19:15 +0900

    fix: broken merge

--- src/ai/backend/storage/xfs/__init__.py
+++ src/ai/backend/storage/xfs/__init__.py
@@ -10,7 +10,6 @@ from uuid import UUID
 from ai.backend.common.logging import BraceStyleAdapter
 from ai.backend.common.types import BinarySize
 
-<<<<<<< HEAD
 from ..exception import ExecutionError, VFolderCreationError
 from ..types import VFolderCreationOptions, VFolderUsage
 from ..vfs import BaseVolume, run
@@ -83,77 +82,12 @@ class XfsProjectRegistry:
             self.name_id_map = {}
             loop = asyncio.get_running_loop()
             raw_projid = await loop.run_in_executor(None, _read_projid_file)
-=======
-from ..exception import (
-    ExecutionError,
-    VFolderCreationError,
-    VFolderNotFoundError,
-)
-from ..types import FSUsage, VFolderCreationOptions
-from ..vfs import BaseVolume
-
-
-async def read_file(loop: asyncio.AbstractEventLoop, filename: str) -> str:
-    def _read():
-        with open(filename, "r") as fr:
-            return fr.read()
-
-    return await loop.run_in_executor(None, lambda: _read())
-
-
-async def write_file(
-    loop: asyncio.AbstractEventLoop,
-    filename: str,
-    contents: str,
-    perm="w",
-):
-    def _write():
-        with open(filename, perm) as fw:
-            fw.write(contents)
-
-    await loop.run_in_executor(None, lambda: _write())
-
-
-async def run(cmd: str) -> str:
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    out, err = await proc.communicate()
-    if err:
-        raise ExecutionError(err.decode())
-    return out.decode()
-
-
-class XfsVolume(BaseVolume):
-    loop: asyncio.AbstractEventLoop
-    registry: Dict[str, int]
-    project_id_pool: List[int]
-
-    async def init(self, uid=None, gid=None, loop=None) -> None:
-        self.registry = {}
-        self.project_id_pool = []
-        if uid is not None:
-            self.uid = uid
-        else:
-            self.uid = os.getuid()
-        if gid is not None:
-            self.gid = gid
-        else:
-            self.gid = os.getgid()
-        self.loop = loop or asyncio.get_running_loop()
-
-        if os.path.isfile("/etc/projid"):
-            raw_projid = await read_file(self.loop, "/etc/projid")
->>>>>>> main
             for line in raw_projid.splitlines():
                 proj_name, proj_id = line.split(":")[:2]
                 project_id_pool.append(int(proj_id))
                 self.name_id_map[UUID(proj_name)] = int(proj_id)
             self.project_id_pool = sorted(project_id_pool)
         else:
-<<<<<<< HEAD
             await run(f"sudo touch {self.file_projid}")
         if not Path(self.file_projects).is_file():
             await run(f"sudo touch {self.file_projects}")
@@ -164,18 +98,6 @@ class XfsVolume(BaseVolume):
         vfid: UUID,
         quota: int,
         project_id: int = None,
-=======
-            await self.loop.run_in_executor(None, lambda: Path("/etc/projid").touch())
-
-        if not os.path.isfile("/etc/projects"):
-            await self.loop.run_in_executor(None, lambda: Path("/etc/projects").touch())
-
-    # ----- volume opeartions -----
-    async def create_vfolder(
-        self,
-        vfid: UUID,
-        options: VFolderCreationOptions = None,
->>>>>>> main
     ) -> None:
         vfpath = self.backend.mangle_vfpath(vfid)
         if project_id is None:
@@ -206,50 +128,17 @@ class XfsVolume(BaseVolume):
             project_id = self.project_id_pool[-1] + 1
         return project_id
 
-<<<<<<< HEAD
 
 class XfsVolume(BaseVolume):
     """
     XFS volume backend. XFS natively supports per-directory quota through
     the project qutoa. To enalbe project quota, the XFS volume should be
     mounted with `-o pquota` option.
-=======
-        vfpath = self.mangle_vfpath(vfid)
-        if options is None or options.quota is None:  # max quota i.e. the whole fs size
-            fs_usage = await self.get_fs_usage()
-            quota = fs_usage.capacity_bytes
-        else:
-            quota = options.quota
-        await self.loop.run_in_executor(
-            None,
-            lambda: vfpath.mkdir(0o755, parents=True, exist_ok=False),
-        )
-        await self.loop.run_in_executor(
-            None,
-            lambda: os.chown(vfpath, self.uid, self.gid),
-        )
-
-        await write_file(
-            self.loop,
-            "/etc/projects",
-            f"{project_id}:{vfpath}\n",
-            perm="a",
-        )
-        await write_file(self.loop, "/etc/projid", f"{vfid}:{project_id}\n", perm="a")
-        await run(f'sudo xfs_quota -x -c "project -s {vfid}" {self.mount_path}')
-        await run(
-            f'sudo xfs_quota -x -c "limit -p bhard={int(quota)} {vfid}" {self.mount_path}',
-        )
-        self.registry[str(vfid)] = project_id
-        self.project_id_pool += [project_id]
-        self.project_id_pool.sort()
->>>>>>> main
 
     This backend requires `root` or no password `sudo` permission to run
     `xfs_quota` command and write to `/etc/projects` and `/etc/projid`.
     """
 
-<<<<<<< HEAD
     registry: XfsProjectRegistry
 
     async def init(self, uid: int = None, gid: int = None) -> None:
@@ -309,54 +198,6 @@ class XfsVolume(BaseVolume):
                     await self.registry.remove_project_entry(vfid)
             await super().delete_vfolder(vfid)
             await self.registry.read_project_info()
-=======
-        await run(
-            f'sudo xfs_quota -x -c "limit -p bsoft=0 bhard=0 {vfid}" {self.mount_path}',
-        )
-
-        raw_projects = await read_file(self.loop, "/etc/projects")
-        raw_projid = await read_file(self.loop, "/etc/projid")
-        new_projects = ""
-        new_projid = ""
-        for line in raw_projects.splitlines():
-            if line.startswith(str(self.registry[str(vfid)]) + ":"):
-                continue
-            new_projects += line + "\n"
-        for line in raw_projid.splitlines():
-            if line.startswith(str(vfid) + ":"):
-                continue
-            new_projid += line + "\n"
-        await write_file(self.loop, "/etc/projects", new_projects)
-        await write_file(self.loop, "/etc/projid", new_projid)
-
-        vfpath = self.mangle_vfpath(vfid)
-
-        def _delete_vfolder():
-            try:
-                shutil.rmtree(vfpath)
-            except FileNotFoundError:
-                pass
-            if not os.listdir(vfpath.parent):
-                vfpath.parent.rmdir()
-            if not os.listdir(vfpath.parent.parent):
-                vfpath.parent.parent.rmdir()
-
-        await self.loop.run_in_executor(None, lambda: _delete_vfolder())
-        self.project_id_pool.remove(self.registry[str(vfid)])
-        del self.registry[str(vfid)]
-
-    async def get_fs_usage(self) -> FSUsage:
-        stat = await run(f"df -h {self.mount_path} | grep {self.mount_path}")
-        if len(stat.split()) != 6:
-            raise ExecutionError("'df -h' stdout is in an unexpected format")
-        _, capacity, used, _, _, path = stat.split()
-        if str(self.mount_path) != path:
-            raise ExecutionError("'df -h' stdout is in an unexpected format")
-        return FSUsage(
-            capacity_bytes=BinarySize.finite_from_str(capacity),
-            used_bytes=BinarySize.finite_from_str(used),
-        )
->>>>>>> main
 
     async def get_quota(self, vfid: UUID) -> BinarySize:
         report = await run(
@@ -374,14 +215,9 @@ class XfsVolume(BaseVolume):
         if vfid not in self.registry.name_id_map.keys():
             await run(f"sudo xfs_quota -x -c 'project -s {vfid}' {self.mount_path}")
         await run(
-<<<<<<< HEAD
             f"sudo xfs_quota -x -c "
             f"'limit -p bsoft={int(size_bytes)} bhard={int(size_bytes)} {vfid}' "
             f"{self.mount_path}"
-=======
-            f'sudo xfs_quota -x -c "limit -p bsoft=0 bhard={int(size_bytes)} {vfid}"'
-            f" {self.mount_path}",
->>>>>>> main
         )
 
     async def get_usage(self, vfid: UUID, relpath: PurePosixPath = None):
