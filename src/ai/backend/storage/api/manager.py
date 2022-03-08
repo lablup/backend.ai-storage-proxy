27de8555c8db5370a673c891f939dad71f0eebf4
Joongi Kim <joongi@lablup.com>
2022-03-08 22:53:39 +0900

    feat: Add the move-file manager-facing API (#40)
    * Preserve rename-file API for backward compatibility
    * Improve propagation of underlying OS errors
      - Strip the vfolder paths to hide the system internals from users

--- src/ai/backend/storage/api/manager.py
+++ src/ai/backend/storage/api/manager.py
@@ -2,9 +2,13 @@
 Manager-facing API
 """
 
+import json
 import logging
+from contextlib import contextmanager as ctxmgr
 from datetime import datetime
-from typing import Awaitable, Callable, List
+from pathlib import Path
+from typing import Awaitable, Callable, Iterator, List
+from uuid import UUID
 
 import attr
 import jwt
@@ -15,6 +19,7 @@ from ai.backend.common import validators as tx
 from ai.backend.common.logging import BraceStyleAdapter
 from ai.backend.storage.exception import ExecutionError
 
+from ..abc import AbstractVolume
 from ..context import Context
 from ..types import VFolderCreationOptions
 from ..utils import check_params, log_manager_api_entry
@@ -43,6 +48,31 @@ async def get_status(request: web.Request) -> web.Response:
             {
                 "status": "ok",
             },
+        )
+
+
+@ctxmgr
+def handle_fs_errors(
+    volume: AbstractVolume,
+    vfid: UUID,
+) -> Iterator[None]:
+    try:
+        yield
+    except OSError as e:
+        related_paths = []
+        if e.filename:
+            related_paths.append(str(volume.strip_vfpath(vfid, Path(e.filename))))
+        if e.filename2:
+            related_paths.append(str(volume.strip_vfpath(vfid, Path(e.filename2))))
+        raise web.HTTPBadRequest(
+            body=json.dumps(
+                {
+                    "msg": e.strerror,
+                    "errno": e.errno,
+                    "paths": related_paths,
+                },
+            ),
+            content_type="application/json",
         )
 
 
@@ -213,16 +243,17 @@ async def fetch_file(request: web.Request) -> web.StreamResponse:
         try:
             prepared = False
             async with ctx.get_volume(params["volume"]) as volume:
-                async for chunk in volume.read_file(
-                    params["vfid"],
-                    params["relpath"],
-                ):
-                    if not chunk:
-                        return response
-                    if not prepared:
-                        await response.prepare(request)
-                        prepared = True
-                    await response.write(chunk)
+                with handle_fs_errors(volume, params["vfid"]):
+                    async for chunk in volume.read_file(
+                        params["vfid"],
+                        params["relpath"],
+                    ):
+                        if not chunk:
+                            return response
+                        if not prepared:
+                            await response.prepare(request)
+                            prepared = True
+                        await response.write(chunk)
         except FileNotFoundError:
             response = web.Response(status=404, reason="Log data not found")
         finally:
@@ -368,12 +399,13 @@ async def mkdir(request: web.Request) -> web.Response:
         await log_manager_api_entry(log, "mkdir", params)
         ctx: Context = request.app["ctx"]
         async with ctx.get_volume(params["volume"]) as volume:
-            await volume.mkdir(
-                params["vfid"],
-                params["relpath"],
-                parents=params["parents"],
-                exist_ok=params["exist_ok"],
-            )
+            with handle_fs_errors(volume, params["vfid"]):
+                await volume.mkdir(
+                    params["vfid"],
+                    params["relpath"],
+                    parents=params["parents"],
+                    exist_ok=params["exist_ok"],
+                )
         return web.Response(status=204)
 
 
@@ -391,23 +423,24 @@ async def list_files(request: web.Request) -> web.Response:
         await log_manager_api_entry(log, "list_files", params)
         ctx: Context = request.app["ctx"]
         async with ctx.get_volume(params["volume"]) as volume:
-            items = [
-                {
-                    "name": item.name,
-                    "type": item.type.name,
-                    "stat": {
-                        "mode": item.stat.mode,
-                        "size": item.stat.size,
-                        "created": item.stat.created.isoformat(),
-                        "modified": item.stat.modified.isoformat(),
-                    },
-                    "symlink_target": item.symlink_target,
-                }
-                async for item in volume.scandir(
-                    params["vfid"],
-                    params["relpath"],
-                )
-            ]
+            with handle_fs_errors(volume, params["vfid"]):
+                items = [
+                    {
+                        "name": item.name,
+                        "type": item.type.name,
+                        "stat": {
+                            "mode": item.stat.mode,
+                            "size": item.stat.size,
+                            "created": item.stat.created.isoformat(),
+                            "modified": item.stat.modified.isoformat(),
+                        },
+                        "symlink_target": item.symlink_target,
+                    }
+                    async for item in volume.scandir(
+                        params["vfid"],
+                        params["relpath"],
+                    )
+                ]
         return web.json_response(
             {
                 "items": items,
@@ -424,25 +457,42 @@ async def rename_file(request: web.Request) -> web.Response:
                 t.Key("vfid"): tx.UUID(),
                 t.Key("relpath"): tx.PurePath(relative_only=True),
                 t.Key("new_name"): t.String(),
-                t.Key("is_dir"): t.ToBool,
+                t.Key("is_dir"): t.ToBool(),  # ignored since 22.03
             },
         ),
     ) as params:
         await log_manager_api_entry(log, "rename_file", params)
         ctx: Context = request.app["ctx"]
-        is_dir = params["is_dir"]
         async with ctx.get_volume(params["volume"]) as volume:
-            if is_dir:
-                await volume.move_tree(
-                    params["vfid"],
-                    params["relpath"],
-                    params["relpath"].with_name(params["new_name"]),
-                )
-            else:
+            with handle_fs_errors(volume, params["vfid"]):
                 await volume.move_file(
                     params["vfid"],
                     params["relpath"],
                     params["relpath"].with_name(params["new_name"]),
+                )
+        return web.Response(status=204)
+
+
+async def move_file(request: web.Request) -> web.Response:
+    async with check_params(
+        request,
+        t.Dict(
+            {
+                t.Key("volume"): t.String(),
+                t.Key("vfid"): tx.UUID(),
+                t.Key("src_relpath"): tx.PurePath(relative_only=True),
+                t.Key("dst_relpath"): tx.PurePath(relative_only=True),
+            },
+        ),
+    ) as params:
+        await log_manager_api_entry(log, "move_file", params)
+        ctx: Context = request.app["ctx"]
+        async with ctx.get_volume(params["volume"]) as volume:
+            with handle_fs_errors(volume, params["vfid"]):
+                await volume.move_file(
+                    params["vfid"],
+                    params["src_relpath"],
+                    params["dst_relpath"],
                 )
         return web.Response(status=204)
 
@@ -535,11 +585,12 @@ async def delete_files(request: web.Request) -> web.Response:
         await log_manager_api_entry(log, "delete_files", params)
         ctx: Context = request.app["ctx"]
         async with ctx.get_volume(params["volume"]) as volume:
-            await volume.delete_files(
-                params["vfid"],
-                params["relpaths"],
-                params["recursive"],
-            )
+            with handle_fs_errors(volume, params["vfid"]):
+                await volume.delete_files(
+                    params["vfid"],
+                    params["relpaths"],
+                    params["recursive"],
+                )
         return web.json_response(
             {
                 "status": "ok",
@@ -571,6 +622,7 @@ async def init_manager_app(ctx: Context) -> web.Application:
     app.router.add_route("POST", "/folder/file/mkdir", mkdir)
     app.router.add_route("POST", "/folder/file/list", list_files)
     app.router.add_route("POST", "/folder/file/rename", rename_file)
+    app.router.add_route("POST", "/folder/file/move", move_file)
     app.router.add_route("POST", "/folder/file/fetch", fetch_file)
     app.router.add_route("POST", "/folder/file/download", create_download_session)
     app.router.add_route("POST", "/folder/file/upload", create_upload_session)
