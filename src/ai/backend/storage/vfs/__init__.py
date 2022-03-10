lablup/backend.ai-storage-proxy

commit 30189fe1566d4ed85b0946a7be8fc4d4c1105c95

feat: Add subpath check to the get_vfolder_mount API for early failure (#41)
* refactor: Reduce duplication and avoid None values where possible
* reafctor: Make the exception hierarchy consistent
* fix: Try to be consistent with error messaging
  - shutil.move() generates two different OSError:
    - Proper errno/strerror values
    - Just a message-only error (which orignates from its Python part
      before calling syscalls)

src/ai/backend/storage/vfs/__init__.py
@@ -134,8 +134,8 @@ class BaseVolume(AbstractVolume):
             ),
         )
 
-    async def get_vfolder_mount(self, vfid: UUID) -> Path:
-        return self.mangle_vfpath(vfid)
+    async def get_vfolder_mount(self, vfid: UUID, subpath: str) -> Path:
+        return self.sanitize_vfpath(vfid, PurePosixPath(subpath))
 
     async def put_metadata(self, vfid: UUID, payload: bytes) -> None:
         vfpath = self.mangle_vfpath(vfid)
@@ -177,7 +177,7 @@ class BaseVolume(AbstractVolume):
     async def get_usage(
         self,
         vfid: UUID,
-        relpath: PurePosixPath = None,
+        relpath: PurePosixPath = PurePosixPath("."),
     ) -> VFolderUsage:
         target_path = self.sanitize_vfpath(vfid, relpath)
         total_size = 0

src/ai/backend/storage/netapp/__init__.py
@@ -280,7 +280,7 @@ class NetAppVolume(BaseVolume):
     async def get_usage(
         self,
         vfid: UUID,
-        relpath: PurePosixPath = None,
+        relpath: PurePosixPath = PurePosixPath("."),
     ) -> VFolderUsage:
         target_path = self.sanitize_vfpath(vfid, relpath)
         total_size = 0
@@ -376,7 +376,7 @@ class NetAppVolume(BaseVolume):
                 loop = asyncio.get_running_loop()
                 await loop.run_in_executor(None, _calc_usage, target_path)
         except StorageProxyError:
-            raise ExecutionError(message="Storage server is busy. Please try again")
+            raise ExecutionError("Storage server is busy. Please try again")
         except FileNotFoundError:
             available = False
         except IndexError:
@@ -387,7 +387,7 @@ class NetAppVolume(BaseVolume):
             total_count = -1
         if not available:
             raise ExecutionError(
-                message="Cannot access the scan result file. Please check xcp is activated.",
+                "Cannot access the scan result file. Please check xcp is activated.",
             )
 
         return VFolderUsage(file_count=total_count, used_bytes=total_size)

src/ai/backend/storage/purestorage/__init__.py
@@ -131,7 +131,7 @@ class FlashBladeVolume(BaseVolume):
     async def get_usage(
         self,
         vfid: UUID,
-        relpath: PurePosixPath = None,
+        relpath: PurePosixPath = PurePosixPath("."),
     ) -> VFolderUsage:
         target_path = self.sanitize_vfpath(vfid, relpath)
         total_size = 0

src/ai/backend/storage/xfs/__init__.py
@@ -237,7 +237,7 @@ class XfsVolume(BaseVolume):
             ],
         )
 
-    async def get_usage(self, vfid: UUID, relpath: PurePosixPath = None):
+    async def get_usage(self, vfid: UUID, relpath: PurePosixPath = PurePosixPath(".")):
         full_report = await run(
             ["sudo", "xfs_quota", "-x", "-c", "report -pbih", self.mount_path],
         )

src/ai/backend/storage/abc.py
@@ -2,19 +2,12 @@ from __future__ import annotations
 
 from abc import ABCMeta, abstractmethod
 from pathlib import Path, PurePath, PurePosixPath
-from typing import (
-    Any,
-    AsyncIterator,
-    Final,
-    FrozenSet,
-    Mapping,
-    Optional,
-    Sequence,
-)
+from typing import Any, AsyncIterator, Final, FrozenSet, Mapping, Sequence
 from uuid import UUID
 
 from ai.backend.common.types import BinarySize, HardwareMetadata
 
+from .exception import InvalidSubpathError, VFolderNotFoundError
 from .types import (
     DirEntry,
     FSPerfMetric,
@@ -57,15 +50,17 @@ class AbstractVolume(metaclass=ABCMeta):
         rest = vfid.hex[4:]
         return Path(self.mount_path, prefix1, prefix2, rest)
 
-    def sanitize_vfpath(self, vfid: UUID, relpath: Optional[PurePosixPath]) -> Path:
-        if relpath is None:
-            relpath = PurePosixPath(".")
+    def sanitize_vfpath(
+        self,
+        vfid: UUID,
+        relpath: PurePosixPath = PurePosixPath("."),
+    ) -> Path:
         vfpath = self.mangle_vfpath(vfid).resolve()
+        if not (vfpath.exists() and vfpath.is_dir()):
+            raise VFolderNotFoundError(vfid)
         target_path = (vfpath / relpath).resolve()
-        try:
-            target_path.relative_to(vfpath)
-        except ValueError:
-            raise PermissionError("cannot access outside of the given vfolder")
+        if not target_path.is_relative_to(vfpath):
+            raise InvalidSubpathError(vfid, relpath)
         return target_path
 
     def strip_vfpath(self, vfid: UUID, target_path: Path) -> PurePosixPath:
@@ -126,7 +121,7 @@ class AbstractVolume(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    async def get_vfolder_mount(self, vfid: UUID) -> Path:
+    async def get_vfolder_mount(self, vfid: UUID, subpath: str) -> Path:
         pass
 
     @abstractmethod
@@ -157,7 +152,7 @@ class AbstractVolume(metaclass=ABCMeta):
     async def get_usage(
         self,
         vfid: UUID,
-        relpath: PurePosixPath = None,
+        relpath: PurePosixPath = PurePosixPath("."),
     ) -> VFolderUsage:
         pass
 

src/ai/backend/storage/api/manager.py
@@ -21,6 +21,7 @@ from ai.backend.storage.exception import ExecutionError
 
 from ..abc import AbstractVolume
 from ..context import Context
+from ..exception import InvalidSubpathError, VFolderNotFoundError
 from ..types import VFolderCreationOptions
 from ..utils import check_params, log_manager_api_entry
 
@@ -60,6 +61,7 @@ def handle_fs_errors(
         yield
     except OSError as e:
         related_paths = []
+        msg = str(e) if e.strerror is None else e.strerror
         if e.filename:
             related_paths.append(str(volume.strip_vfpath(vfid, Path(e.filename))))
         if e.filename2:
@@ -67,7 +69,7 @@ def handle_fs_errors(
         raise web.HTTPBadRequest(
             body=json.dumps(
                 {
-                    "msg": e.strerror,
+                    "msg": msg,
                     "errno": e.errno,
                     "paths": related_paths,
                 },
@@ -187,13 +189,39 @@ async def get_vfolder_mount(request: web.Request) -> web.Response:
             {
                 t.Key("volume"): t.String(),
                 t.Key("vfid"): tx.UUID(),
+                t.Key("subpath", default="."): t.String(),
             },
         ),
     ) as params:
         await log_manager_api_entry(log, "get_container_mount", params)
         ctx: Context = request.app["ctx"]
         async with ctx.get_volume(params["volume"]) as volume:
-            mount_path = await volume.get_vfolder_mount(params["vfid"])
+            try:
+                mount_path = await volume.get_vfolder_mount(
+                    params["vfid"],
+                    params["subpath"],
+                )
+            except VFolderNotFoundError:
+                raise web.HTTPBadRequest(
+                    body=json.dumps(
+                        {
+                            "msg": "VFolder not found",
+                            "vfid": str(params["vfid"]),
+                        },
+                    ),
+                    content_type="application/json",
+                )
+            except InvalidSubpathError as e:
+                raise web.HTTPBadRequest(
+                    body=json.dumps(
+                        {
+                            "msg": "Invalid vfolder subpath",
+                            "vfid": str(params["vfid"]),
+                            "subpath": str(e.args[1]),
+                        },
+                    ),
+                    content_type="application/json",
+                )
             return web.json_response(
                 {
                     "path": str(mount_path),

src/ai/backend/storage/exception.py
@@ -4,26 +4,24 @@ from typing import Any
 from aiohttp import web
 
 
-class ExecutionError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-
 class StorageProxyError(Exception):
     pass
 
 
-class VFolderCreationError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
+class ExecutionError(StorageProxyError):
+    pass
 
 
-class VFolderNotFoundError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
+class VFolderCreationError(StorageProxyError):
+    pass
+
+
+class VFolderNotFoundError(StorageProxyError):
+    pass
+
+
+class InvalidSubpathError(StorageProxyError):
+    pass
 
 
 class InvalidVolumeError(StorageProxyError):

commit 0c4df3a2d265c99b805d03a866a23536b2b96e80

hotfix: A simple regression due to refactoring in #41

src/ai/backend/storage/vfs/__init__.py
@@ -135,7 +135,8 @@ class BaseVolume(AbstractVolume):
         )
 
     async def get_vfolder_mount(self, vfid: UUID, subpath: str) -> Path:
-        return self.sanitize_vfpath(vfid, PurePosixPath(subpath))
+        self.sanitize_vfpath(vfid, PurePosixPath(subpath))
+        return self.mangle_vfpath(vfid).resolve()
 
     async def put_metadata(self, vfid: UUID, payload: bytes) -> None:
         vfpath = self.mangle_vfpath(vfid)