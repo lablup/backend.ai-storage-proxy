529b3415c9f8342505ebfaf3ece2ae2c1eebc6fe
Sergey Leksikov <lexikovs@gmail.com>
2022-03-18 18:24:08 +0900

    feat: add container network monitor

---
+++ src/ai/backend/storage/containerMonitor.py
@@ -0,0 +1,85 @@
+import asyncio
+import aiodocker
+import math
+
+async def get_filebrowsers():
+
+    docker = aiodocker.Docker()
+    container_list = []
+
+    containers = await aiodocker.docker.DockerContainers(docker).list()
+
+    for container in containers:
+
+        stats = await container.stats(stream=False)
+        print(container._id, stats)
+        name = stats[0]['name']
+        cnt_id = stats[0]['id']
+
+        if "FileBrowser" in name:
+            container_list.append(cnt_id)
+
+    await docker.close()
+
+    return container_list
+
+
+async def destroy_container(container_id):
+    docker = aiodocker.Docker()
+    container = aiodocker.docker.DockerContainer(docker, id=container_id)
+    await container.stop()
+    await container.delete()
+    await docker.close()
+    print("done")
+    return 1
+
+
+async def network_monitor(container_id, freq, period, threshold):
+
+    network_window = []
+    docker = aiodocker.Docker()
+    container = aiodocker.docker.DockerContainer(docker, id=container_id)
+
+
+
+
+    while True:
+
+        stats = await container.stats(stream=False)
+
+        network_total_transfer = stats[0]['networks']['eth0']['rx_bytes'] + stats[0]['networks']['eth0']['tx_bytes']
+
+        network_window.append(network_total_transfer)
+        print("network total: ", network_total_transfer)
+
+        if len(network_window) > period:
+            network_utilization_change = network_window[-1] - network_window[0]
+            if network_utilization_change == 0:
+                await destroy_container(container_id)
+                await docker.close()
+                break
+
+        asyncio.sleep(freq)
+    return 1
+
+
+async def main():
+    monitored_list = []
+    while True:
+
+        browsers = await get_filebrowsers()
+        print(browsers)
+
+        if len(browsers) > 0:
+            for browser in browsers:
+                if browser not in monitored_list:
+                    monitored_list.append(browser)
+                    asyncio.create_task(network_monitor(browser, 1, 10, 1024))
+
+        asyncio.sleep(10)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
