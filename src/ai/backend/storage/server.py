5bdf2ebca8c9ae77d833e6ca43541b023138cba9
lexikovs <lexikovs@gmail.com>
2022-04-25 16:02:37 +0900

    fix: linter issues

--- src/ai/backend/storage/server.py
+++ src/ai/backend/storage/server.py
@@ -149,6 +149,7 @@ async def server_main(
         if ctx.local_config["filebrowser"]["db_path"].is_file():
             ctx.local_config["filebrowser"]["db_path"].unlink()
 
+
 @click.group(invoke_without_command=True)
 @click.option(
     "-f",
@@ -239,7 +240,6 @@ def main(cli_ctx, config_path, debug):
             if monitor_lock_path.exists() and file_lock.locked:
                 file_lock.release()
                 monitor_lock_path.unlink()
-            
 
     return 0
 
